06d61e5520f136c999fc7ced3f889ee25e9ae8fe
Akiomi Kamakura <akiomik@gmail.com>
2022-07-08 21:42:48 +0900

    Initial commit

---
+++ lib/scenic/dependencies.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "dependencies/version"
+
+module Scenic
+  module Dependencies
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
